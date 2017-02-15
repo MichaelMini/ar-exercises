232df37c7527fbe6ac733b40f2acd72d996807f1
MichaelMini <mcho911@gmail.com>
2017-02-15 05:00:48 +0000

    having a problem with @store3

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,15 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+# p @store3
+@store1.employees.create(first_name: "Michael", last_name: "Cho", hourly_rate: 60)
+@store1.employees.create(first_name: "Sam", last_name: "Sung", hourly_rate: 60)
+@store1.employees.create(first_name: "So", last_name: "ny", hourly_rate: 60)
+@store2.employees.create(first_name: "lksdfj", last_name: "ef", hourly_rate: 60)
+@store2.employees.create(first_name: "ksjdf", last_name: "Virani", hourly_rate: 60)
+# @store3.employees.create(first_name: "sdf", last_name: "ef", hourly_rate: 60)
+# @store3.employees.create(first_name: "sdfsa", last_name: "we", hourly_rate: 60)
+@store4.employees.create(first_name: "sfs", last_name: "weg", hourly_rate: 60)
+@store4.employees.create(first_name: "eeew", last_name: "efwe", hourly_rate: 60)
+@store5.employees.create(first_name: "sfw", last_name: "grd", hourly_rate: 60)
+@store6.employees.create(first_name: "gder", last_name: "eg", hourly_rate: 60)
