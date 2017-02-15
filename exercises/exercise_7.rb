9cb4e7d7891f7b9939c36026f8826d53beba45fa
MichaelMini <mcho911@gmail.com>
2017-02-15 20:29:53 +0000

    exercise 7 is done

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,12 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+# get user's input for a store name
+# create new store with that name from user
+p 'Please give it a store name and press enter'
+store_name = gets.chomp
+@store7 = Store.create(name: "#{store_name}")
+@store7.errors.messages.each do | k, error |
+  p "#{k} #{error.join(', ')}"
+end
+# p Employee
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,14 @@
 class Store < ActiveRecord::Base
   has_many :employees
+  validates :name, presence: true, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  validate :men_or_women_apparel_must_be_presence
+
+  def men_or_women_apparel_must_be_presence
+    if mens_apparel == false && womens_apparel == false
+      errors.add(:mens_apparel, "mens or womens apparel must presence")
+    end
+  end
+
 end
